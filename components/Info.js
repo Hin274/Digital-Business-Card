968145f03ddd02bf9912bb8cbc0f892a76fbc568
ChongHinYu <mailtohin274@hotmail.com>
2023-02-14 00:58:21 +0000

    Add onclick to email and linkedin

--- components/Info.js
+++ components/Info.js
@@ -5,12 +5,19 @@ export default function Info() {
         <header>
             <div className="container-grid">
                 <img src="./images/Profile-Picture.jpg" className="info--pic" />
-                    <div>
+                <div>
                     <h1 className="heading">Chong Hin Yu</h1>
                     <h3 className="sub-heading">Junior Frontend Developer</h3>
                     <h4><a href="https://github.com/Hin274">https://github.com/Hin274</a></h4>
                 </div>
-                <div className="container-flex"><button><i className="fa-solid fa-envelope"></i>Email</button><button className="button--blue"><i className="fa-brands fa-linkedin"></i>LinkedIn</button></div>
+                <div className="container-flex">
+                    <button onclick="location.href='mailto:';">
+                        <i className="fa-solid fa-envelope"></i>Email
+                    </button>
+                    <button onclick="window.open('https://www.linkedin.com/in/chong-hin-yu-152912229','blank')" type="button" className="button--blue">
+                        <i className="fa-brands fa-linkedin"></i>LinkedIn
+                    </button>
+                </div>
             </div>
         </header>
     )
